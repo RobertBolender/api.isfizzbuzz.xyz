e696b31e45914a42d13144506ef78ac8bbec8666
Robert Bolender <robert@bolender.email>
2024-08-28 09:57:50 -0700

    Fix logging and error checking for ListenAndServe

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	_ "embed"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"log"
 	"net/http"
@@ -92,9 +93,13 @@ func main() {
 		fmt.Fprint(w, indexHTML)
 	})
 
-	http.ListenAndServe(":8080", nil)
-
-	log.Println("Listening on :8080")
+	log.Println("Starting server on :8080")
+	err := http.ListenAndServe(":8080", nil)
+	if errors.Is(err, http.ErrServerClosed) {
+		log.Println("Server closed")
+	} else if err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
 }
 
 func isFizzBuzz(n int) bool {
